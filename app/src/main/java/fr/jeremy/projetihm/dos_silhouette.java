8402d324c1e850aceb387c7ecaab92652afb0c68
jeremyraine <jeremy.raine@edu.esiee.fr>
2021-10-29 17:01:04 +0200

    page_principale a jour

--- app/src/main/java/fr/jeremy/projetihm/dos_silhouette.java
+++ app/src/main/java/fr/jeremy/projetihm/dos_silhouette.java
@@ -2,13 +2,39 @@ package fr.jeremy.projetihm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class dos_silhouette extends AppCompatActivity {
+
+    private Button button_retour;
+    private Button button_profil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dos_silhouette);
+
+        button_retour = (Button) findViewById(R.id.button_retour);
+        button_profil = (Button) findViewById(R.id.button_profil);
+
+        button_retour.setOnClickListener(v -> {
+            openpage_principale();
+        });
+
+        button_profil.setOnClickListener(v -> {
+            openprofil();
+        });
+    }
+
+    public void openpage_principale() {
+        Intent intent = new Intent(this, page_principale.class);
+        startActivity(intent);
+    }
+
+    public void openprofil() {
+        Intent intent = new Intent(this, profil.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/fr/jeremy/projetihm/page_principale.java
+++ app/src/main/java/fr/jeremy/projetihm/page_principale.java
@@ -5,11 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class page_principale extends AppCompatActivity {
 
     private Button button_dos;
     private Button button_profil;
+    private Button button_pec;
+    private Button button_epaules;
+    private Button button_epaules2;
+    private Button button_biceps;
+    private Button button_biceps2;
+    private Button button_abdos;
+    private Button button_quadri;
+    private Button button_quadri2;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +29,47 @@ public class page_principale extends AppCompatActivity {
         setContentView(R.layout.activity_page_principale);
         button_dos = (Button) findViewById(R.id.button_dos);
         button_profil  = (Button) findViewById(R.id.button_profil);
+        button_pec  = (Button) findViewById(R.id.button_pec);
+        button_epaules = (Button) findViewById(R.id.button_epaules);
+        button_epaules2 = (Button) findViewById(R.id.button_epaules2);
+        button_biceps = (Button) findViewById(R.id.button_biceps);
+        button_biceps2 = (Button) findViewById(R.id.button_biceps2);
+        button_abdos = (Button) findViewById(R.id.button_abdos);
+        button_quadri = (Button) findViewById(R.id.button_quadri);
+        button_quadri2 = (Button) findViewById(R.id.button_quadri2);
+
+
+
 
         button_dos.setOnClickListener(v -> {
             opendos_silhouette();
         });
         button_profil.setOnClickListener(v -> {
             openprofil();
+        });
+        button_pec.setOnClickListener(v -> {
+            openpectoraux();
+        });
+        button_epaules.setOnClickListener(v -> {
+            openepaules();
+        });
+        button_epaules2.setOnClickListener(v -> {
+            openepaules();
+        });
+        button_biceps.setOnClickListener(v -> {
+            openbiceps();
+        });
+        button_biceps2.setOnClickListener(v -> {
+            openbiceps();
+        });
+        button_abdos.setOnClickListener(v -> {
+            openabdos();
+        });
+        button_quadri.setOnClickListener(v -> {
+            openquadri();
+        });
+        button_quadri2.setOnClickListener(v -> {
+            openquadri();
         });
     }
 
@@ -32,6 +79,26 @@ public class page_principale extends AppCompatActivity {
     }
     public void openprofil() {
         Intent intent = new Intent(this, profil.class);
+        startActivity(intent);
+    }
+    public void openpectoraux() {
+        Intent intent = new Intent(this, pectoraux.class);
+        startActivity(intent);
+    }
+    public void openepaules() {
+        Intent intent = new Intent(this, epaules.class);
+        startActivity(intent);
+    }
+    public void openbiceps() {
+        Intent intent = new Intent(this, biceps.class);
+        startActivity(intent);
+    }
+    public void openabdos() {
+        Intent intent = new Intent(this, abdos.class);
+        startActivity(intent);
+    }
+    public void openquadri() {
+        Intent intent = new Intent(this, quadri.class);
         startActivity(intent);
     }
 }
