435b6d7b45d9efebacecf3f85682e186ffc75a03
jeremyraine <jeremy.raine@edu.esiee.fr>
2021-12-16 10:06:14 +0100

    chart operationnels

--- app/src/main/java/fr/jeremy/projetihm/Fragment2.java
+++ app/src/main/java/fr/jeremy/projetihm/Fragment2.java
@@ -1,19 +1,39 @@
 package fr.jeremy.projetihm;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Fragment2 extends Fragment {
+public class Fragment2 extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_2, container, false);
+
+        Spinner spinner = (Spinner) getView().findViewById(R.id.spinner_pec);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Exercices, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String choice = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getActivity(), choice, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
